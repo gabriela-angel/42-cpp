f03c83885de8c4e87496da47ab21f558ae2f65a2
gangel-a <gangel-a@student.42sp.org.br>
2025-06-24 15:44:46 -0300

    starts structuring ex00

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 14:03:16 by gangel-a          #+#    #+#             */
-/*   Updated: 2025/06/24 14:42:08 by gangel-a         ###   ########.fr       */
+/*   Updated: 2025/06/24 15:41:26 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int main(int argc, char **argv)
 {
-	char *str = argv[1];
+	int i = 1;
 
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (argv[i])
+		{
+			std::cout << argv[i];
+			i++;
+		}
+	}
+	std::cout << "\n";
+	return (0);
 }
