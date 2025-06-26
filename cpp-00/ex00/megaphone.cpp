95ccaddad8b237deed1c33acd1a08e51fa38f58d
gangel-a <gangel-a@student.42sp.org.br>
2025-06-26 15:00:06 -0300

    completes ex00

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 14:03:16 by gangel-a          #+#    #+#             */
-/*   Updated: 2025/06/24 15:41:26 by gangel-a         ###   ########.fr       */
+/*   Updated: 2025/06/26 14:57:38 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int main(int argc, char **argv)
 {
-	int i = 1;
-
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		while (argv[i])
+		for (int i = 1; argv[i] != '\0'; i++)
 		{
-			std::cout << argv[i];
-			i++;
+			for (int j = 0; argv[i][j] != '\0'; j++)
+				std::cout << (char)toupper(argv[i][j]);
 		}
 	}
-	std::cout << "\n";
+	std::cout << std::endl;
 	return (0);
 }
