80dff3ffe58a1c597b5c1b1f7e1cb9d23675954e
gangel-a <gangel-a@student.42sp.org.br>
2025-07-01 16:29:42 -0300

    .

--- cpp-00/ex01/main.cpp
+++ cpp-00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 14:07:03 by gangel-a          #+#    #+#             */
-/*   Updated: 2025/06/27 18:52:04 by gangel-a         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:01:07 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	main(void)
 {
-	PhoneBook	phonebook;
 	Contact		contacts[8];
+	PhoneBook	phonebook(contacts);
 	std::string	cmd;
+	int i = 0;
 
-	while (!cmd || cmd != "EXIT")
+	while (true)
 	{
 		std::cout << "Enter a command from the following list [ADD, SEARCH, EXIT]: ";
 		std::cin >> cmd;
-		if (cmd == "ADD")
+		if (cmd.compare("ADD") == 0)
 		{
 			std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
 			std::cout << "\nFirst name: ";
@@ -36,12 +37,18 @@ int	main(void)
 			std::cout << "\nDarkest secret: ";
 			std::cin >> darkestSecret;
 			std::cout << std::endl;
+			if (i == 8)
+				i = 0;
+			if (phonebook.contacts[i].modifyContact(firstName, lastName,
+				nickName, phoneNumber, darkestSecret) == 0)
+				i++;
 		}
-		else if (cmd == "SEARCH")
+		else if (cmd.compare("SEARCH") == 0)
 		{
-			std::cout << "Index     |First name|Last name |Nickname  ";
-
+			std::cout << "     Index|First name| Last name|  Nickname";
 		}
+		else if (cmd.compare("EXIT") == 0)
+			break ;
 	}
 	return (0);
 }
--- cpp-00/ex01/phonebook.class.hpp
+++ cpp-00/ex01/phonebook.class.hpp
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 14:03:33 by gangel-a          #+#    #+#             */
-/*   Updated: 2025/06/27 18:32:33 by gangel-a         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:00:37 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,17 @@ public:
 
 	Contact(void);
 	~Contact(void);
+
+	int modifyContact(std::string firstName,
+		std::string lastName, std::string nickname,
+		std::string phoneNumber, std::string darkestSecret);
 };
 
 class	PhoneBook {
 public:
 	Contact	contacts[8];
 
-	PhoneBook(void);
+	PhoneBook(Contact list_contact[8]);
 	~PhoneBook(void);
 };
 
